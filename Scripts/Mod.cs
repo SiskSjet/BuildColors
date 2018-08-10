995b431703224275286b8ebbeed65ad7d8ebe181
Sisk S'jet <sisk.sjet@gmail.com>
2018-08-10 12:49:55 +0200

    feat: save player colors on world save

--- Scripts/Mod.cs
+++ Scripts/Mod.cs
@@ -1,7 +1,37 @@
-﻿using VRage.Game.Components;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sandbox.ModAPI;
+using Sisk.BuildColors.Settings;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
 
 namespace Sisk.BuildColors {
     [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
     public class Mod : MySessionComponentBase {
+        private const string SETTINGS_FILE = "settings.xml";
+
+        /// <summary>
+        ///     Save mod settings.
+        /// </summary>
+        // bug?: SaveData is called after the game save. You can use GetObjectBuilder if you need.
+        public override void SaveData() {
+            SavePlayerColors();
+        }
+
+        /// <summary>
+        ///     Save player colors.
+        /// </summary>
+        private void SavePlayerColors() {
+            var players = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(players, x => !x.IsBot);
+
+            var settings = new ModSettings {
+                Colors = players.Select(x => new PlayerColors { Id = x.SteamUserId, Colors = MyAPIGateway.Utilities.SerializeToBinary(x.BuildColorSlots) }).ToArray()
+            };
+
+            using (var writer = MyAPIGateway.Utilities.WriteFileInWorldStorage(SETTINGS_FILE, typeof(Mod))) {
+                writer.Write(MyAPIGateway.Utilities.SerializeToXML(settings));
+            }
+        }
     }
 }
---
+++ Scripts/Settings/PlayerColors.cs
@@ -0,0 +1,14 @@
+﻿using ProtoBuf;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Sisk.BuildColors.Settings {
+    [ProtoContract]
+    public class PlayerColors {
+        [ProtoMember(2)]
+        public byte[] Colors { get; set; }
+
+        [ProtoMember(1)]
+        public ulong Id { get; set; }
+    }
+}
